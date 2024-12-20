e1e47b103c0b7033b119c0b26b29b2c76eaa83d1
Katie <ktpham115@student.hbuhsd.edu>
2024-12-20 18:21:05 +0000

    4.4 day 2(?)

--- homework.cpp
+++ homework.cpp
@@ -4,16 +4,47 @@
 int main()
 {
     // Complete the homework problems here
+
+    //problem 1
+
+    int num;
+    std::cout << "Enter a number from 1-100.\n";
+    std::cin >> num;
+    while(num <1 || num > 100)
+    {
+        std::cout << "Invalid input.\n";
+        std::cin >> num;
+    }
+    std::cout << "Thank you for your input.\n";
+
+    //problem 2
+    std::string favColor = "black";
+    std::string hmm;
+    int tries = 1;
+    std::cout << "Guess the color.\n";
+    std::cin >> hmm;
+    while(hmm != favColor)
+    {
+        std::cout << "loud incorrect buzzer. Try again.\n";
+        std::cin >> hmm;
+        tries = tries + 1;
+    }
+    std::cout << "Correct. It took " << tries << " tries.\n";
+
+    //problem 3
+    int count;
+    int sum = 0;
     
-    // Problem 1
-    
-    
-    // Problem 2
-    
-    
-    // Problem 3
-    
-    
-    
+    std::cout << "How many numbers to sum up?\n";
+    std::cin >> count;
+    for (int i = 1; i <= count; i = i+1)
+    {
+        int num;
+        std::cout << "Enter number " << i << ".\n";
+        std::cin >> num;
+        sum = sum + num;
+    }
+    std::cout << "The sum is: " << sum << "\n";
+
     return 0;
 }
